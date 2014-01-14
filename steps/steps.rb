8f3f6f6f8e74c0feda09cd72c2e38532f07cd3a6
Josef Salyer <josef.salyer@jpmchase.com>
2014-01-14 11:14:25 -0500

    added some basic steps

---
+++ steps/steps.rb
@@ -0,0 +1,19 @@
+Given(/^I have a Suc$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am an Authorized User$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I can see the Login Form$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I enter my Credentials$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the Login Success View$/) do
+  pending # express the regexp above with the code you wish you had
+end
